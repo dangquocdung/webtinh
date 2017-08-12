6ae30222aee7a21a0f77414c206d45c6d44fb9bb
Quoc Dung Dang <quocdungdang@Quocs-MacBook-Pro.local>
2017-08-12 21:35:22 +0700

    ss

--- resources/views/guest/layout/main.blade.php
+++ resources/views/guest/layout/main.blade.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html>
-@include('guest.layout.head')
-
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="./images/favicon.ico">
+  @yield('title')
+  <base href="{{asset('')}}">
+  <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="./assets/css/app.css">
+  <script type="text/javascript" src="./js/jquery.min.js"></script>
+  <script type="text/javascript" src="./js/bootstrap.min.js" ></script>
+  <!-- Danang -->
+  <link rel="stylesheet" type="text/css" href="./css/header-menu.css">
+  <link rel="stylesheet" type="text/css" href="./css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="./css/style.css">
+  <link rel="stylesheet" type="text/css" href="./css/mentions.css">
+  <link rel="stylesheet" type="text/css" href="./css/main.css">
+</head>
 <body>
-
   @include('guest.layout.header-banner')
   @include('guest.layout.main-menu')
   @include('guest.layout.menu-mobile')
   @yield('content')
   @include('guest.layout.footer')
 </body>
-
 </html>
